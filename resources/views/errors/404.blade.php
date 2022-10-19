04396aeffdc7660182d1134f9bb3a224952dc691
vitaly <vitalicheg28@mail.ru>
2022-10-19 18:25:01 +0300

    404

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,58 @@
+<!doctype html>
+<html lang="en" class="no-focus">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+
+    <title>Codebase - Bootstrap 4 Admin Template &amp; UI Framework</title>
+
+    <meta name="description" content="Codebase - Bootstrap 4 Admin Template &amp; UI Framework created by pixelcave and published on Themeforest">
+    <meta name="author" content="pixelcave">
+    <meta name="robots" content="noindex, nofollow">
+
+    <meta property="og:title" content="Codebase - Bootstrap 4 Admin Template &amp; UI Framework">
+    <meta property="og:site_name" content="Codebase">
+    <meta property="og:description" content="Codebase - Bootstrap 4 Admin Template &amp; UI Framework created by pixelcave and published on Themeforest">
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="">
+    <meta property="og:image" content="">
+
+    <link rel="shortcut icon" href="{{ URL::asset('media/favicons/m-circle.png') }}">
+    <link rel="icon" type="image/png" sizes="192x192" href="{{ URL::asset('media/favicons/m-circle.png') }}">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ URL::asset('media/favicons/apple-touch-icon-180x180.png') }}">
+
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito+Sans:300,400,400i,600,700&display=swap">
+    <link rel="stylesheet" id="css-main" href="{{ URL::asset('css/codebase.min.css') }}">
+</head>
+<body>
+
+<div id="page-container" class="main-content-boxed">
+
+    <main id="main-container">
+
+        <div class="hero bg-white">
+            <div class="hero-inner">
+                <div class="content content-full">
+                    <div class="py-30 text-center">
+                        <div class="display-3 text-danger">
+                            <i class="fa fa-warning"></i> 404
+                        </div>
+                        <h1 class="h2 font-w700 mt-30 mb-10">Oops.. You just found an error page..</h1>
+                        <h2 class="h3 font-w400 text-muted mb-50">We are sorry but the page you are looking for was not found..</h2>
+                        <a class="btn btn-hero btn-rounded btn-alt-secondary" href="{{ url()->previous() }}">
+                            <i class="fa fa-arrow-left mr-10"></i> Go back
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </main>
+
+</div>
+
+<script src="{{ URL::asset('js/codebase.core.min.js') }}"></script>
+
+<script src="{{ URL::asset('js/codebase.app.min.js') }}"></script>
+</body>
+</html>
